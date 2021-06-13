979801c1dd0e4bb22f42872402cc1f99748a6137
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-06-13 11:06:35 +0000

    base for all the components I will create

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,49 @@
-<script context="module" lang="ts">
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
 	export const prerender = true;
 </script>
 
-<script lang="ts">
-	import Counter from '$lib/Counter/index.svelte';
-</script>
-
 <svelte:head>
-	<title>Home</title>
+	<title>About</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+<div class="content">
+	<h1>Svelte Componet</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<p>
+    This project hold svelete commponets that built because I found the intreting on
+    daily UI and uplabs
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+	</p>
 
-	<Counter />
-</section>
+	<!-- TODO lose the @next! -->
+	<pre>git checkout "componet-name"</pre>
+
+	<p>
+        The componet are seperated by git branches
+	</p>
+
+	<p>
+		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
+		it with JavaScript disabled!
+	</p>
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
+	.content {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
 	}
 </style>
