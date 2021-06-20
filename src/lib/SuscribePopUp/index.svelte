c792152cebe823d2b8081bcf611be152b0df8f58
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-06-20 16:52:15 +0000

    add suscribe pop up ui

---
+++ src/lib/SuscribePopUp/index.svelte
@@ -0,0 +1,72 @@
+<script lang="ts" charset="utf-8">
+</script>
+
+<div class="container">
+    <button class="btn-close">x</button>
+	<h3>Enjoyed the read?</h3>
+	<p>Join our monthly newsletter for helpful tips on design and more.</p>
+
+	<form action="" method="get" class="form">
+		<div class="form-group">
+			<label hidden for="email">Enter your email: </label>
+			<input class='email-input' placeholder="Your email address" type="email" name="email" id="email" required />
+		</div>
+		<div class="form-group">
+			<input type="checkbox" id="scales" name="scales" checked />
+			<label for="scales">I agree to receive this newsletter and subscribe at any time.</label>
+		</div>
+            <button class="btn-submit" type="submit" >Subscribe</button>
+	</form>
+</div>
+
+<style>
+
+    .container {
+        margin: 0 auto;
+        width: 30rem;
+        padding: 2rem;
+        background-color: rgba(255, 255, 255, .73);
+        display: grid;
+        justify-items: center;
+        border-radius: 1rem;
+    }
+    .btn-close {
+        background-color: white;
+        border: none;
+        display: block;
+        font-size: 1.5rem;
+        /* padding: .5rem; */
+        width: 2.5rem;
+        height: 2.5rem;
+        line-height: 0;
+        border-radius: 50%;
+        justify-self: end;
+        align-self: center;
+    }
+
+    h3, p {
+        color: #151936;
+        text-align: center;
+        width: 70%;
+        margin: 0;
+    }
+    .form {
+        width: 80%;
+        padding: 2rem 0 4rem 0;
+        display: grid;
+        grid-gap: 1rem;
+    }
+    .form .email-input {
+        width: 100%;
+        padding: 0.862rem;
+        border-radius: 5px;
+        border: solid 1px #A7AAC9;
+    }
+    .btn-submit {
+        background: #2D2865;
+        border-radius: 5px;
+        padding: 0.862rem;
+        border: none;
+    }
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	import { browser, dev } from '$app/env';
+	import SuscribePopUp from '$lib/SuscribePopUp/index.svelte';
 
 	// we don't need any JS on this page, though we'll load
 	// it in dev so that we get hot module replacement...
@@ -18,32 +19,44 @@
 	<title>About</title>
 </svelte:head>
 
-<div class="content">
-	<h1>Svelte Componet</h1>
-
-	<p>
-    This project hold svelete commponets that built because I found the intreting on
-    daily UI and uplabs
-
-	</p>
-
-	<!-- TODO lose the @next! -->
-	<pre>git checkout "componet-name"</pre>
-
-	<p>
-        The componet are seperated by git branches
-	</p>
-
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
-	</p>
+<div class="container">
+	<span class="oval oval-big" />
+	<span class="oval oval-small" />
+	<SuscribePopUp />
 </div>
 
 <style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
+	.container {
+		position: relative;
+        min-height: 72vh;
+        /* background-color: red; */
+        display: grid;
+        align-content: center;
+	}
+
+	.oval {
+		border-radius: 50%;
+		position: absolute;
+		display: block;
+		z-index: -1;
+		filter: blur(1rem);
+	}
+
+	.oval.oval-small {
+		height: 12rem;
+		width: 12rem;
+		background: linear-gradient(90deg, #ffd7ae, #ffb3af);
+        transform: translate(-50%,-50%);
+        top: 65%;
+        left: 30%;
+	}
+
+	.oval.oval-big {
+		background: linear-gradient(45deg, #aadbff, #ffb3af);
+        top: 40%;
+        left: 60%;
+		height: 16rem;
+        transform: translate(-50%,-50%);
+		width: 16rem;
 	}
 </style>
